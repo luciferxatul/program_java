e6cb394f194cef17171cebb3bea2f4a103c822ad
canggihwr <canggihrinaldi8@gmail.com>
2022-10-06 16:40:07 +0800

    Find ASCII value

---
+++ ASSCIIvalue.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kalkulatorbmi;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author cwr
+ */
+
+public class AsciiValue {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        char value = 'a';
+        int ascii = value;
+        // 2nd method : You can also cast char to int
+        int castAscii = (int) ch;
+
+        System.out.println("The ASCII value of " + value + " is: " + ascii);
+        System.out.println("The ASCII value of " + value + " is: " + castAscii);
+    }
+}
